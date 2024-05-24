6137bcc9d00d0bf0254edb8af097096f40bfbafd
SayedAli <sayedxali7@gmail.com>
2024-05-24 21:12:00 +0330

    enhance: modified swagger documentation for project

--- src/main/java/com/seyed/ali/TaskService/controller/TaskController.java
+++ src/main/java/com/seyed/ali/TaskService/controller/TaskController.java
@@ -5,7 +5,13 @@ import com.seyed.ali.TaskService.model.payload.Result;
 import com.seyed.ali.TaskService.model.payload.TaskDTO;
 import com.seyed.ali.TaskService.service.interfaces.TaskService;
 import com.seyed.ali.TaskService.util.converter.TaskConverter;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,12 +25,14 @@ import static org.springframework.http.HttpStatus.OK;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/task")
 @SecurityRequirement(name = "Keycloak")
+@Tag(name = "Task Operation", description = "API for operation on task.")
 public class TaskController {
 
     private final TaskService taskService;
     private final TaskConverter taskConverter;
 
     @PostMapping
+    @Operation(summary = "Create a task", description = "Adds a new task to the database")
     public ResponseEntity<Result> createTask(@RequestBody TaskDTO taskDTO) {
         Task task = this.taskConverter.convertToTask(taskDTO);
         Task createdTask = this.taskService.createTask(task);
@@ -39,6 +47,13 @@ public class TaskController {
     }
 
     @GetMapping("/{projectId}")
+    @Operation(summary = "Get all task for project", description = "Fetches all tasks for associated project from the database", responses = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successful operation",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = TaskDTO.class)))
+            )
+    })
     public ResponseEntity<Result> findAllTasksForProject(@PathVariable String projectId) {
         List<Task> taskList = this.taskService.findAllTasksForProject(projectId);
         List<TaskDTO> response = this.taskConverter.convertTaskDTOList(taskList);
